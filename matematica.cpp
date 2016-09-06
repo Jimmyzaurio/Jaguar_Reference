942ffd145fe2041df9d8b84cc8826b38163b9b1b
Jimmyzaurio <jsp.saucedo@gmail.com>
2016-09-06 13:27:25 -0500

    Update matematica.cpp

--- matematica.cpp
+++ matematica.cpp
@@ -23,14 +23,18 @@ vector<Factor> FactoresPrimos(int a) {
     return factores;
 }
 
-// Criba de Eratostenes de 1 a n.
-vector<int> Criba(int n) {
-    int raiz = sqrt(n); vector<int> criba(n + 1);
-    for (int i = 4; i <= n; i += 2) criba[i] = 2;
-    for (int i = 3; i <= raiz; i += 2) if (!criba[i])
-        for (int j = i * i; j <= n; j += i)
-            if (!criba[j]) criba[j] = i;
-    return criba;
+// Criba competitiva
+bitset<10000010> bs;
+vi primes;
+
+void sieve(ll upper) {
+    ll tam = upper + 1;
+    bs.set();
+    bs[0] = bs[1] = 0;
+    for (ll i = 2; i <= tam; i++) if (bs[i]) {
+        for (ll j = i*i; j <= tam; j += i) bs[j] = 0;
+        primes.push_back((int)i);
+    }
 }
 
 Long get_powers(Long n, Long p) {
