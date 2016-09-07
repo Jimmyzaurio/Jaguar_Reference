770760c06ef7e618edd2dde1fbc8ae4ccd4cfc6e
Jimmyzaurio <jsp.saucedo@gmail.com>
2016-09-07 06:25:17 -0500

    Create Jaguar.cpp

---
+++ Jaguar.cpp
@@ -0,0 +1,18 @@
+Red Black Tree
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+typedef tree<
+int, null_type,
+less<int>, rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+ordered_set X;
+X.insert(1);
+
+// Kth largest element
+cout<< *X.find_by_order(1)
+// Number of smallest elements than key
+cout << X.order_of_key(-5);
