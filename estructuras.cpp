15939cf444c7d4a29396774e348db1d14bfc622e
Jimmyzaurio <jsp.saucedo@gmail.com>
2016-09-09 21:58:47 -0500

    Update estructuras.cpp

--- estructuras.cpp
+++ estructuras.cpp
@@ -235,6 +235,17 @@ struct BIT3D {
     }
 };
 
+struct union_find {
+    vi p; union_find(int n) : p(n, -1) { }
+    int find(int x) { return p[x] < 0 ? x : p[x] = find(p[x]); }
+    bool unite(int x, int y) {
+        int xp = find(x), yp = find(y);
+        if (xp == yp) return false;
+        if (p[xp] > p[yp]) swap(xp,yp);
+        p[xp] += p[yp], p[yp] = xp;
+        return true; }
+    int size(int x) { return -p[find(x)]; } };
+
 // Trie 
 // (t->sig.find(i) != t->sig.end())
 vector<char> resp;
