2273542fe527ed9b868082780888dcfc4f4016a6
Jesús Saucedo <jsp.saucedo@gmail.com>
2016-10-15 19:35:20 -0500

    Update Jaguar.cpp

--- Jaguar.cpp
+++ Jaguar.cpp
@@ -3,6 +3,9 @@ Red Black Tree
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
+using namespace std;
+using namespace __gnu_pbds;
+
 typedef tree<
 int, null_type,
 less<int>, rb_tree_tag,
